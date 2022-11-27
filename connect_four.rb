b4875e30850110a57f36db8bbe9d09a5274c8c79
Brian-Momata <brianmomata@gmail.com>
2022-11-27 09:29:39 +0300

    Fix styling as per rubocop

--- connect_four.rb
+++ connect_four.rb
@@ -1,4 +1,4 @@
-#contains intro, game rules and the messages for if there's a winner
+# contains intro, game rules and the messages for if there's a winner
 module Game
   def intro
     puts "Connect Four is a 2-player board game, in which the players take turns
@@ -13,7 +13,7 @@ or diagonal line of four of one's own tokens.\n\n"
   end
 
   def draw
-   puts 'Draw'
+    puts 'Draw'
   end
 end
 
@@ -23,10 +23,10 @@ class GameBoard
   attr_accessor :board
 
   def initialize
-    @board = Array.new(6) { Array.new(7) { "\e[37m \u26AA"} }
+    @board = Array.new(6) { Array.new(7) { "\e[37m \u26AA" } }
     intro
   end
-  
+
   def display_board
     label = ' 1  2  3  4  5  6  7'
     puts "#{board[0][0]}#{board[0][1]}#{board[0][2]}#{board[0][3]}#{board[0][4]}#{board[0][5]}#{board[0][6]}"
@@ -55,8 +55,7 @@ class GameBoard
     loop do
       column = verify_input(player_input(player))
       break if column
-     
-      puts "input error"
+      puts 'input error'
     end
     column - 1
   end
@@ -66,7 +65,7 @@ class GameBoard
   end
 
   def winner?(player)
-    #horiontal check
+    # horiontal check
     board.each do |row|
       row.each_index do |j|
         if row[j] == player.token && row[j + 1] == player.token && row[j + 2] == player.token && row[j + 3] == player.token
@@ -75,7 +74,7 @@ class GameBoard
       end
     end
 
-    #vertical check
+    # vertical check
     board.each_with_index do |row, i|
       row.each_index do |j|
         break if i > 2
@@ -85,22 +84,23 @@ class GameBoard
       end
     end
 
-    #diagonal check
+    # diagonal check
     board.each_with_index do |row, i|
       row.each_index do |j|
         break if i > 2
-        if j < 4 #left to right diagonal
+
+        if j < 4 # left to right diagonal
           if board[i][j] == player.token && board[i + 1][j + 1] == player.token && board[i + 2][j + 2] == player.token && board[i + 3][j + 3] == player.token
             return true
           end
-        elsif j > 2 #right to left diagonal
+        elsif j > 2 # right to left diagonal
           if board[i][j] == player.token && board[i + 1][j - 1] == player.token && board[i + 2][j - 2] == player.token && board[i + 3][j - 3] == player.token
             return true
           end
         end
       end
     end
-    return false
+    false
   end
 
   def play_game(p1, p2)
@@ -119,6 +119,7 @@ class GameBoard
         p1.turn = true
       end
       break if board.all? { |row| row.none? { |token| token == "\e[37m \u26AA" } }
+
       winner_message(p1) if winner?(p1)
       winner_message(p2) if winner?(p2)
     end
@@ -126,14 +127,15 @@ class GameBoard
   end
 
   private
+
   def player_input(player)
     puts "#{player.name}: Choose a column to place token"
     gets.chomp.to_i
   end
 end
 
-#class for the players
-class Player 
+# class for the players
+class Player
   attr_reader :name, :token
   attr_accessor :turn
   
@@ -146,9 +148,9 @@ class Player
 
   def get_name
     if empty?
-      puts "Player 1: What is your name?"
+      puts 'Player 1: What is your name?'
     else
-      puts "Player 2: What is your name?"
+      puts 'Player 2: What is your name?'
     end
     player_name = gets.chomp
     @@players << player_name
@@ -157,6 +159,7 @@ class Player
 
   def get_token
     return "\e[33m \u26AB" if size == 1
+
     "\e[31m \u26AB"
   end
 
